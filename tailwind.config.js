1a6476192ed5cd76b0604640d7d4aeaf36a65592
Natalia Carvalho <ssspinosaure@gmail.com>
2023-04-03 16:29:43 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        teagreen: "#CCD5AE",
+        beige: "#E9EDC9",
+        cornsilk: "#FEFAE0",
+        papayawhip: "#FAEDCD",
+        buff: "#D4A373",
+      },
+    },
+  },
+  plugins: [],
+};
